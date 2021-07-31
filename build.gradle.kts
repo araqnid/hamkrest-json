c298368247ee4aee63387a58449567ade2ca4bd0
Steve Haslam <araqnid@gmail.com>
2021-07-31 23:28:17 +0100

    Kotlin 1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.net.URI
 
 plugins {
-    kotlin("jvm") version "1.3.61"
+    kotlin("jvm") version "1.4.30"
     `maven-publish`
     signing
 }
